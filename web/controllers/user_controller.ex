5a92795eea1dee996c47af2ba909634a7b64bb25
Dmitry Harnitski <dmitry@brainjocks.com>
2016-09-04 23:38:06 -0400

    authenticate controller

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -3,13 +3,23 @@ defmodule Rumbl.UserController do
   alias Rumbl.User
 
   def index(conn, _params) do
-    users = Repo.all(User)
-    render conn, "index.html", users: users
+    case authenticate(conn) do
+      %Plug.Conn{halted: true} = conn ->
+        conn
+      conn ->
+        users = Repo.all(User)
+        render conn, "index.html", users: users
+    end
   end
 
-  def show(conn, %{"id" => id}) do
-    user = Repo.get(User, id)
-    render conn, "show.html", user: user
+  def show(conn, %{"id" => id}) 
+    case authenticate(conn) do
+      %Plug.Conn{halted: true} = conn ->
+        conn
+      conn ->
+        user = Repo.get(User, id)
+        render conn, "show.html", user: user
+    end
   end
 
   def new(conn, _params) do
@@ -27,6 +37,17 @@ defmodule Rumbl.UserController do
 
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
     end
   end
 
