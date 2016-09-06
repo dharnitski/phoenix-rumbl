aac222355f31f1415974008e11da9cf1ad872391
Dmitry Harnitski <dmitry@brainjocks.com>
2016-09-06 00:01:03 -0400

    create Category structures p109

--- web/models/video.ex
+++ web/models/video.ex
@@ -6,16 +6,17 @@ defmodule Rumbl.Video do
     field :title, :string
     field :description, :string
     belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
 
     timestamps()
   end
 
-  @doc """
-  Builds a changeset based on the `struct` and `params`.
-  """
+  @required_fields ~w(url title description)
+  @optional_fields ~w(category_id)
+
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:url, :title, :description])
-    |> validate_required([:url, :title, :description])
+    |> cast(params, @required_fields, @optional_fields)
   end
+  
 end
