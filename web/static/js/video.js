b08abafdf3d55401f2f322aad7fa7cbab57a56a2
Dmitry Harnitski <dmitry@brainjocks.com>
2016-09-20 10:55:20 -0500

    join video channel - p181

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -4,12 +4,26 @@ let Video = {
 
   init(socket, element) {
     if (!element) { return }
-    let msgContainer = document.getElementById("msg-container")
-    let msgInput = document.getElementById("msg-input")
-    let postButton = document.getElementById("msg-submit")
     let videoId = element.getAttribute("data-id")
     let playerId = element.getAttribute("data-player-id")
+    socket.connect()
+    //todo: code from book does not wark - call back nmethod is not fired
+    //Player.init(element.id, playerId, () => {
+    //  this.onReady(videoId, socket)
+    //})
     Player.init(element.id, playerId)
+    this.onReady(videoId, socket)
+  },
+
+  onReady(videoId, socket){
+    let msgContainer = document.getElementById("msg-container")
+    let msgInput     = document.getElementById("msg-input")
+    let postButton   = document.getElementById("msg-submit")
+    let vidChannel   = socket.channel("videos:" + videoId)
+
+    vidChannel.join()
+      .receive("ok", resp => console.log("joined the video channel", resp))
+      .receive("error", reason => console.log("join failed", reason))
   }
 }
 
