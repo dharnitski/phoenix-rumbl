7b78be31fa94ad7699024d2d026379fcd962ccc0
Dmitry Harnitski <dmitry@brainjocks.com>
2016-09-20 11:47:20 -0500

    annotate videos p185

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -2,8 +2,18 @@ defmodule Rumbl.VideoChannel do
   use Rumbl.Web, :channel
 
   def join("videos:" <> video_id, _params, socket) do
-    :timer.send_interval(5_000, :ping)
+    #:timer.send_interval(5_000, :ping)
     {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+  end
+
+  def handle_in("new_annotation", params, socket) do
+    broadcast! socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    }
+
+    {:reply, :ok, socket}
   end
 
   def handle_info(:ping, socket) do
--- web/static/js/video.js
+++ web/static/js/video.js
@@ -23,9 +23,43 @@ let Video = {
 
     vidChannel.on("ping", ({count}) => console.log("PING", count))
 
+    postButton.addEventListener("click", e => {
+      let payload = {body: msgInput.value, at: Player.getCurrentTime()}
+      vidChannel.push("new_annotation", payload)
+                .receive("error", e => console.log(e))
+      msgInput.value = ""
+    })
+
+    vidChannel.on("new_annotation", (resp) => {
+      this.renderAnnotation(msgContainer, resp)
+    })
+
     vidChannel.join()
       .receive("ok", resp => console.log("joined the video channel", resp))
       .receive("error", reason => console.log("join failed", reason))
+  },
+
+  renderAnnotation(msgContainer, {user, body, at}) {
+    let template = document.createElement("div")
+    template.innerHTML = `
+    <a href="#" data-seek=${this.esc(at)}>[${this.formatTime(at)}]
+      <b>${this.esc(user.username)}</b>: ${this.esc(body)}
+    </a>
+    `
+    msgContainer.appendChild(template)
+    msgContainer.scrollTop = msgContainer.scrollHeight
+  },
+
+  esc(str){
+    let div = document.createElement("div")
+    div.appendChild(document.createTextNode(str))
+    return div.innerHTML
+  },
+
+  formatTime(at) {
+    let date = new Date(null)
+    date.setSeconds(at / 1000)
+    return date.toISOString().substr(14, 5)
   }
 }
 
